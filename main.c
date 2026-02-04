4b5c55404d41b6d030626f0e51df4762a4c79ed8
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-02-05 00:57:49 +0200

    added a linear allocator

--- main.c
+++ main.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+
 
 #define CAPACITY 640000
 
 char heap[CAPACITY] = {0};
+size_t used_size = 0;
+
 
 void *heap_allocate(size_t size){
 
-    return NULL;
+    assert( used_size + size <= CAPACITY);
+    void *result = heap + used_size;
+    used_size += size ;
+
+    return result;
 }
 
 void heap_free(void *ptr){
-
-
+    (void) ptr;
+    
 }
 
 
-int main(){
-    
+int main(void)
+{
+    // test 
+    char *mem = heap_allocate(26);
+
+    for (int i = 0;i < 26; ++i){
+        mem[i] = i + 'A';
+    }
+
+    printf("%s", mem);
+    return 0 ; 
 }
