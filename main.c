3907053620e7fc778c168bd5f8401cd1fbad41cb
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-02-04 23:24:42 +0200

    added main functions

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define CAPACITY 640000
+
+char heap[CAPACITY] = {0};
+
+void *heap_allocate(size_t size){
+
+    return NULL;
+}
+
+void heap_free(void *ptr){
+
+
+}
+
+
+int main(){
+    
+}
